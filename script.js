9596503ea5f06249577b044acd83c7fecc0a6c27
sid161 <sharma.siddharth@gmail.com>
2024-03-06 10:53:07 +0530

    update feature

--- script.js
+++ script.js
@@ -1,43 +1,61 @@
-
 const malaCountElement = document.getElementById('malaCount');
 const pointsContainer = document.getElementById('points');
-
+const mantraElement = document.getElementById('mantra');
 
 const totalPoints = 108;
 
-
 let counter = 0;
 let malaCounter = 0;
+let mantraVisible = false;
 
+// Create Rudraksha points
 for (let i = 0; i < totalPoints; i++) {
     const point = document.createElement('div');
     point.className = 'rudraksha-point';
     pointsContainer.appendChild(point);
+    point.style.display = 'none'; 
 }
 
 // Tap function
 function tap() {
     const points = document.getElementsByClassName('rudraksha-point');
     if (counter < totalPoints) {
-        points[counter].style.display = 'none';
+        points[counter].style.display = 'inline-block';
         counter++;
         if (counter === totalPoints) {
             updateMalaCount();
             resetPoints();
         }
     }
+    toggleMantraVisibility(); 
+    updateTapCounter();
+    setTimeout(toggleMantraVisibility, 1600); 
 }
 
-// Update mala 
+// Update tap counter
+function updateTapCounter() {
+    const tapCounterElement = document.getElementById('tapCounter');
+    tapCounterElement.textContent = counter;
+}
+
+// Update mala count
 function updateMalaCount() {
     malaCounter++;
     malaCountElement.textContent = malaCounter;
 }
 
+// Reset points
 function resetPoints() {
     counter = 0;
     const points = document.getElementsByClassName('rudraksha-point');
     for (let i = 0; i < points.length; i++) {
-        points[i].style.display = 'inline-block';
+        points[i].style.display = 'none'; // Hide all Rudraksha points
     }
 }
+
+// Toggle mantra visibility
+function toggleMantraVisibility() {
+    const mantraText = document.getElementById('mantraText');
+    mantraText.style.display = mantraText.style.display === 'none' ? 'block' : 'none'; // Toggle display property
+}
+
